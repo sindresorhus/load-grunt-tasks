c5babe58e96eada1dcd7b94556918f9322e9b783
Sindre Sorhus <sindresorhus@gmail.com>
2014-12-23 00:07:17 +0700

    various minor tweaks

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,6 @@
 'use strict';
 module.exports = function (grunt) {
-	require('./load-grunt-tasks')(grunt, {
+	require('./')(grunt, {
 		pattern: ['*'],
 		config: require('./package'),
 		scope: 'devDependencies'
