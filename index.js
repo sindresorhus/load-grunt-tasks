83055606f065d3ad723241934d6b302954289418
Kevin Martensson <kevinmartensson@gmail.com>
2015-07-14 13:27:44 +0200

    Use `arrify`

--- index.js
+++ index.js
@@ -2,17 +2,14 @@
 var path = require('path');
 var findup = require('findup-sync');
 var multimatch = require('multimatch');
-
-function arrayify(el) {
-	return Array.isArray(el) ? el : [el];
-}
+var arrify = require('arrify');
 
 module.exports = function (grunt, opts) {
 	opts = opts || {};
 
-	var pattern = arrayify(opts.pattern || ['grunt-*', '@*/grunt-*']);
+	var pattern = arrify(opts.pattern || ['grunt-*', '@*/grunt-*']);
 	var config = opts.config || findup('package.json');
-	var scope = arrayify(opts.scope || ['dependencies', 'devDependencies', 'peerDependencies', 'optionalDependencies']);
+	var scope = arrify(opts.scope || ['dependencies', 'devDependencies', 'peerDependencies', 'optionalDependencies']);
 
 	if (typeof config === 'string') {
 		config = require(path.resolve(config));
