153c15646db0628e46ea51c7865a7a78f0368e80
Andrew Petersen <kirbysayshi@gmail.com>
2015-10-28 17:53:04 +0100

    Use node_modules resolution algo instead of broken grunt behavior

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var path = require('path');
 var pkgUp = require('pkg-up');
 var multimatch = require('multimatch');
 var arrify = require('arrify');
+var resolve = require('require-resolve');
 
 module.exports = function (grunt, opts) {
 	opts = opts || {};
@@ -22,5 +23,8 @@ module.exports = function (grunt, opts) {
 		return result.concat(Array.isArray(deps) ? deps : Object.keys(deps));
 	}, []);
 
-	multimatch(names, pattern).forEach(grunt.loadNpmTasks);
+	multimatch(names, pattern).forEach(function (pkgName) {
+		var resolved = resolve(path.join(pkgName, 'package.json'));
+		grunt.loadTasks(path.join(resolved.pkg.root, 'tasks'));
+	});
 };
