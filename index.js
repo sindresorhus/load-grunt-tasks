a5dab2341a320e2210e74baadb39c5b5e8acde0f
philippsimon <github@philippsimon.de>
2016-03-01 13:26:24 +0700

    Close #45 PR: Fix requireResolution with not found task.

--- index.js
+++ index.js
@@ -25,7 +25,11 @@ module.exports = function (grunt, opts) {
 
 	multimatch(names, pattern).forEach(function (pkgName) {
 		if (opts.requireResolution === true) {
-			grunt.loadTasks(resolvePkg(path.join(pkgName, 'tasks')));
+			try {
+				grunt.loadTasks(resolvePkg(path.join(pkgName, 'tasks')));
+			} catch (e) {
+				grunt.log.error('Npm module "' + pkgName + '" not found. Is it installed?');
+			}
 		} else {
 			grunt.loadNpmTasks(pkgName);
 		}
