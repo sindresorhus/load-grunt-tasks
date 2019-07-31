6234bc9874511c759c952fe9f6467298217a00e8
nunospot <the@nunospot.com>
2019-07-31 01:05:37 +0100

    Print a useful error if package.json is missing (#61)
    Co-authored-by: Sindre Sorhus <sindresorhus@gmail.com>

--- index.js
+++ index.js
@@ -11,6 +11,11 @@ module.exports = (grunt, options = {}) => {
 
 	let cwd = process.cwd();
 	let config = options.config || pkgUp.sync();
+
+	if (config === null) {
+		grunt.fail.fatal('package.json not found. Make sure to create a package.json and install dependencies before you run Grunt.');
+	}
+
 	if (typeof config === 'string') {
 		const configPath = path.resolve(config);
 		cwd = path.dirname(configPath);
